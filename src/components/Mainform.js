49b74ee4412c35dd8c4fa92ba914495ac6168f2a
kira <529280602@qq.com>
2016-11-22 18:06:19 +0800

    post

---
+++ src/components/Mainform.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Mainform.css';
+
+function Mainform(props) {
+  return (
+    <div className={styles.normal}>
+      Component: 'Mainform'
+    </div>
+  );
+}
+
+export default Mainform;
